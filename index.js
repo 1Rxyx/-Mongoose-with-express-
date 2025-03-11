c76398418bd524350d5404b023fcafb41582ed50
1Rxyx <ttooaa231011@gmail.com>
2025-03-12 03:41:18 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const Product = require("./models/product");
+
+mongoose
+  .connect("mongodb://localhost:27017/farmStand")
+  .then(() => {
+    console.log("MongoDBコネクションOK！！");
+  })
+  .catch((err) => {
+    console.log("MongoDBコネクションエラー！！！");
+    console.log(err);
+  });
+
+//ミドルウェア系
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+const categories = ["果物", "野菜", "乳製品"];
+
+//一覧ページ
+app.get("/products", async (req, res) => {
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index", { products, category: '全'});
+  }
+});
+
+//新規作成
+app.get("/products/new", async (req, res) => {
+  res.render("products/new", { categories });
+});
+
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+//詳細ページ
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  console.log(product);
+  res.render("products/show", { product });
+});
+
+//編集ページ
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
+
+app.listen(3000, () => {
+  console.log("ぽーと3000で待受中...");
+});
